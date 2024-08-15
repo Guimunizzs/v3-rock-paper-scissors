787d6aca3044232a9c83e6763895e5bca400cb31
Guilherme Muniz <gui.munizalemao@gmail.com>
2024-08-15 14:45:26 -0300

    Update index.js

--- rockpaper/src/index.js
+++ rockpaper/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -15,3 +16,10 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+ReactDOM.render(
+  <Router basename="/rock-paper-scissors">
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
